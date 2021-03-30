db8b6cecbd633d796f68d906c44025676af40387
Lorf91 <57013254+Lorf91@users.noreply.github.com>
2021-03-30 20:54:36 +0300

    start

---
+++ js/script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+window.addEventListener("DOMContentLoaded", () => {
+
+    const hederNavItems = document.querySelectorAll('.header__nav .item');
+
+    hederNavItems[hederNavItems.length - 1].style.borderRight = 0; //Убераем крайний правый border в последнем item nav
+
+    
+});

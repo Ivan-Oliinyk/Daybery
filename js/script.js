1a2f7ef7cae833996f31843b7cac62d4d5e4987d
Lorf91 <57013254+Lorf91@users.noreply.github.com>
2021-03-31 23:37:44 +0300

    modal window for block servises

--- js/script.js
+++ js/script.js
@@ -6,5 +6,60 @@ window.addEventListener("DOMContentLoaded", () => {
 
     hederNavItems[hederNavItems.length - 1].style.borderRight = 0; //Убераем крайний правый border в последнем item nav
 
+    //функция для открытия закрытия модельных окон блока Услуги
+    function runModalWindows() {
+        const modalContents = document.querySelectorAll('.modal_wrapper'),
+              closeModal = document.querySelectorAll('.modal__content .close'),
+              btnServise = document.querySelectorAll('.btn_servise'),
+              parentContent = document.querySelector('.servises__content');
+
+        function hideModal() {
+            modalContents.forEach(item => {
+                item.classList.add('hide');
+                item.classList.remove('show', 'fade');
+            });
+
+            document.body.style.overflow = 'auto';
+        }
+
+        hideModal();
+
+        function showModal(i) {
+            modalContents[i].classList.add('show', 'fade');
+            modalContents[i].classList.remove('hide');
+            document.body.style.overflow = 'hidden';
+        }
+
+        btnServise.forEach((item, i) => {
+            item.addEventListener('click', () => {
+                hideModal();
+                showModal(i);
+            });
+        })
+
+        closeModal.forEach(item => {
+            item.addEventListener('click', hideModal);
+        });
+
+        //закрываем модельное окно при клике esc
+        document.addEventListener('keydown', (e) => {
+            modalContents.forEach(modal => {
+                if (e.code === "Escape" && modal.classList.contains('show')) {
+                    hideModal();
+                }
+            });
+        });
+
+        //закрываем модельное окно при клике на подложку
+        modalContents.forEach(modal => {
+            modal.addEventListener('click', (e) => {
+                if (e.target === modal) {
+                    hideModal();
+                }
+            });
+        });
+    }
     
+    runModalWindows();
+
 });

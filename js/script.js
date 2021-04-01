f2abf08b4ec8dd5d8da3833442f9dac4e84d49bc
Lorf91 <57013254+Lorf91@users.noreply.github.com>
2021-04-02 00:26:34 +0300

    slider-begin

--- js/script.js
+++ js/script.js
@@ -62,4 +62,50 @@ window.addEventListener("DOMContentLoaded", () => {
     
     runModalWindows();
 
+    //функция сдайдера
+    function runSlider() {
+        const slides = document.querySelectorAll('.offer__slide'),
+              prev = document.querySelector('.offer__slider-prev'),
+              next = document.querySelector('.offer__slider-next'),
+              slidesWrapper = document.querySelector('.offer__slider-wrapper'),
+              slidesField = document.querySelector('.offer__slider-inner'),
+            //   width = window.getComputedStyle(slides[0]).width;
+              width = window.getComputedStyle(slidesWrapper).width;
+        
+        let offset = 0;
+    
+        slidesField.style.width = 33 * slides.length + '%';
+        slidesField.style.display = 'flex';
+        slidesField.style.transition = '0.5s all';
+    
+        slidesWrapper.style.overflow = 'hidden';
+    
+        slides.forEach(slide => {
+            slide.style.width = width;
+        });
+    
+        next.addEventListener('click', () => {
+            
+            if (offset == +width.slice(0, width.length -2) * (slides.length - 1)) {
+                offset = +width.slice(0, width.length -2) * (slides.length - 1);
+            } else {
+                offset += +width.slice(0, width.length - 2);
+            }
+    
+            slidesField.style.transform =`translateX(-${offset/3}px)`;
+        });
+    
+        prev.addEventListener('click', () => {
+            if (offset == 0) {
+                offset = 0;
+            } else {
+                offset -= +width.slice(0, width.length - 2);
+            }
+    
+            slidesField.style.transform =`translateX(-${offset/3}px)`;
+        });
+    }
+
+    runSlider();
+
 });
